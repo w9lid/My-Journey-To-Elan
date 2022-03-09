50b42aa960531ed74d2714093169df8b4ddeb12e
w9lid <95339373+w9lid@users.noreply.github.com>
2022-03-09 13:46:41 +0100

    Delete 7. One page Wexim directory

--- 7. One page Wexim/js/script.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){
-    // Au click sur la div.menu, nous activerons et désactiverons la classe .active pour la nav mais aussi pour la div.menu
-    $(".menu").click(function() {
-        $(".menu").toggleClass("active");
-        $(".nav").toggleClass("active");
-        });
-});
